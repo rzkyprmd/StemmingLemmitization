f65e1618d811ca73c50d05203aa86b877298d030
Risky Pramudia <rzkyprmd666@gmail.com>
2021-12-31 11:48:29 +0700

    tugas

---
+++ Stemming.py
@@ -0,0 +1,11 @@
+
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+  
+ps = PorterStemmer()
+ 
+
+words = ["program", "programs", "programmer", "programming", "programmers"]
+ 
+for w in words:
+    print(w, " : ", ps.stem(w))
